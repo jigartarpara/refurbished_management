f16fd6b26b1454d973e8ee57aa455052a728b58e
Jigar Tarpara <jigartarpara@khatavahi.in>
2025-07-24 11:39:21 +0530

    Foundation

---
+++ refurbished_management/refurbished_management/doctype/refurb_order/test_refurb_order.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, jigartarpara and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRefurbOrder(FrappeTestCase):
+	pass
