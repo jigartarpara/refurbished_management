d06465a035e9e71911c2d56228b30fd5f9c66dee
= <=>
2025-07-25 13:19:13 +0530

    Git Config Check

--- refurbished_management/refurbished_management/doctype/refurb_order/refurb_order.py
+++ refurbished_management/refurbished_management/doctype/refurb_order/refurb_order.py
@@ -17,7 +17,6 @@ class RefurbOrder(Document):
         result = {}
         result["device_transfer_entry"] = True if transfer > 0 else False
         result["net_transfer_stock"] = self.get_net_transfer()
-
         return result
     
     def get_net_transfer(self):
